92c2afc6666ba863f5cd12302946d08d6f104667
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-04-17 20:16:15 +0800

    add pseudo code

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,29 @@
 package sg.edu.nus.iss;
 
 /**
- * Hello world!
+ * Task:
+ * Write a Java console program that allows a user to add, remove and list the contents of a shopping card.
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Print welcome message
+
+        // Allow user to enter keyboard input
+
+        // List logic
+        // If input is "list" and list is length 0 print "Your cart is empty"
+        // If list has items, print "1. FRUIT \n 2. FRUIT2 \n..."
+
+        // Add fruit logic
+        // If added FRUIT already exists, print "You have FRUIT in your cart"
+        // If input is "add FRUIT", print "FRUIT added to cart"
+        // If more than one FRUIT e.g. add orange, pear, print above on individual lines. CHECK FOR COMMAS TO SPLIT INPUT
+
+        // Delete fruit logic
+        // If specified index does not exist, print "Incorrect item index"
+        // if input is "delete INT", remove fruit at specified index and print "FRUIT_AT_INDEX removed from cart"
     }
 }
