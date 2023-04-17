0a89406cfb3dc06f15839946cb35e01e81ada72a
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-04-17 23:10:32 +0800

    add ability to show deleted fruit

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -25,42 +25,53 @@ public class App
         // String userInput = cons.readLine("Type in a command to begin: 'list', 'add', 'remove' - \n");
         try (Scanner scan = new Scanner(System.in)) {
             while(!scan.hasNext("exit")){
-            String commandGiven = scan.next();
-            // If input is "list" and list is length 0 print "Your cart is empty"
-            if (commandGiven.equals("list")) {
-                if (cart.size() == 0) {
-                    System.out.print("~ Its empty lmao.\n");
+                String commandGiven = scan.next();
+                // If input is "list" and list is length 0 print "Your cart is empty"
+                if (commandGiven.equals("list")) {
+                    if (cart.size() == 0) {
+                        System.out.print("~ Its empty lmao.\n");
+                    }
+                    // If list has items, print "1. FRUIT \n 2. FRUIT2 \n..."
+                    else {
+                        for (int i = 0; i < cart.size(); i++) {
+                            System.out.printf("%d. %s\n", (i+1), cart.get(i));
+                        }
+                    }
                 }
-                // If list has items, print "1. FRUIT \n 2. FRUIT2 \n..."
-                else {
-                    for (int i = 0; i < cart.size(); i++) {
-                        System.out.printf("%d. %s", (i+1), cart.get(i));
+
+                // Add fruit logic
+                if (commandGiven.equals("add")) {
+                    // If added FRUIT already exists, print "You have FRUIT in your cart"
+                    // loop through fruit array to see which fruits alr exist and add those that are not
+                    if (scan.hasNextLine()) {
+                        String[] tempFruitArray = scan.nextLine().trim().split(", ");
+                        for (String fruit: tempFruitArray) {
+                            if (cart.contains(fruit)) {
+                                System.out.printf("~ You already have %s in your cart :P\n", fruit);
+                            }
+                            else {
+                                // If input is "add FRUIT", print "FRUIT added to cart"
+                                cart.add(fruit);
+                                System.out.printf("~ YUMMY %s has been added to your cart!\n", fruit);
+                            }
+                        }
+                    }
+                }
+                // Delete fruit logic
+                if (commandGiven.equals("remove")) {
+                    Integer inputIndex = Integer.parseInt(scan.nextLine().trim());
+                    Integer idx = inputIndex - 1;
+                    // If specified index does not exist, print "Incorrect item index"
+                    if (idx > (cart.size() + 1) || idx < 0) {
+                        System.out.print("~ Item index does not exist\n");
+                    }
+                    else {
+                        cart.remove(1);
+                        System.out.printf("~ %s removed from cart\n", cart.get(idx));
                     }
                 }
             }
-
-            // Add fruit logic
-            if (commandGiven.equals("add")) {
-                // If added FRUIT already exists, print "You have FRUIT in your cart"
-                // loop through fruit array to see which fruits alr exist and add those that are not
-                if (scan.hasNextLine()) {
-                    String[] tempFruitArray = scan.nextLine().trim().split(", ");
-                    for (String fruit: tempFruitArray) {
-                        if (cart.contains(fruit)) {
-                            System.out.printf("~ You already have %s in your cart :P\n", fruit);
-                        }
-                        else {
-                            // If input is "add FRUIT", print "FRUIT added to cart"
-                            cart.add(fruit);
-                            System.out.printf("~ YUMMY %s has been added to your cart!\n", fruit);
-                        }
-                    }
-                }
-            }}
     
-            // Delete fruit logic
-            // If specified index does not exist, print "Incorrect item index"
-            // if input is "delete INT", remove fruit at specified index and print "FRUIT_AT_INDEX removed from cart"
         }
         catch (Exception e) {
             System.out.println("~ Error: " + e);
